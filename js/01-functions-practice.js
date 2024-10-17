7546269b25f1c8900d2fd8053355dccd2a694389
lil-melon <133168636+lil-melon@users.noreply.github.com>
2024-10-16 19:08:51 -0700

    updated output

--- js/01-functions-practice.js
+++ js/01-functions-practice.js
@@ -1,29 +1,29 @@
 //STEP 1
 const halfNumber = num=>{
     let strResult = "Half of " + num + " is " + num/2 +"."
-    console.log(strResult)
     return strResult
 }
 
 //STEP 2
 const squareNumber = num => {
     let strResult = "The result of squaring the number " + num + " is " + num*num + "."
-    console.log(strResult)
     return strResult
 }
 
 //STEP 3
 const percentOf = (num1,num2)=>{
     let strResult = num1 + " is " + Math.round((num1/num2*100),2) + "% of " + num2
-    console.log(strResult)
     return strResult
 } 
 
 //STEP 4
 const findModulus = (num1,num2)=>{
     let strResult = num2%num1 + " is the modulus of " + num1 + " and " + num2
-    console.log(strResult)
     return strResult
 }
 
 //STEP 5
+console.log(halfNumber(5))
+console.log(squareNumber(5))
+console.log(percentOf(5,10))
+console.log(findModulus(5,10))
